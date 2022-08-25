92278ad37b0641d4421a91b4bed2b1c1288a09d8
gefic <112091647+gefic@users.noreply.github.com>
2022-08-25 16:57:04 -0300

    Add files via upload
    Inicialización del repositorio para la primer entrega

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edita <code>src/App.js</code> guardalo y luego reinicia.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprende React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
