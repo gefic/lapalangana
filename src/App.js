88ba32c8174afd17fae31e6415c351733c85c26c
gefic <112091647+gefic@users.noreply.github.com>
2022-09-01 16:25:16 -0300

    Update App.js
    Se actualiza la aplicación App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/'
+import Carrito from './components/Carrito/'
 
 function App() {
   return (
     <NavBar/>
+    <Carrito/>
   );
 }
 
