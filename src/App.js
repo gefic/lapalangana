28f4634773319d4ecbd7073a2137f812b401f912
gefic <112091647+gefic@users.noreply.github.com>
2022-09-06 22:32:33 -0300

    Update App.js
    Ajusto nombres

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/'
-import CarWidget from './components/CarWidget/'
+import Cart from './components/Cart/'
 
 function App() {
   return (
     <NavBar/>
-    <CarWidget/>
+    <Cart/>
   );
 }
 
