a85e3284f7fe69e14111856f4612aaba13cadfed
gefic <112091647+gefic@users.noreply.github.com>
2022-09-15 23:09:28 -0300

    Add files via upload
    Actualizo y ajusto página

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ItemListContainer from './components/itemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/itemDetailContainer/ItemDetailContainer';
 import Navbar from './components/navbar/Navbar';
-
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar/>
-        {/* <center><ItemListContainer greeting="Sitio de venta de discos"/></center> */}
-        {/* <ItemDetailContainer /> */}
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/lapalangana' element={<ItemListContainer />} />
+        <Route path='/category/:idCategory' element={<ItemListContainer />} />
+        <Route path='/item/:idItem' element={<ItemDetailContainer /> }/>
+        <Route path='/cart' element={<Cart /> }/>
+
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
